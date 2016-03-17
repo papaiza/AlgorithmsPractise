737a8b11b4e36e139b6b464ee33f8c63fd6c81d3
papaiza <anton@uniswipe.com>
2016-03-17 11:18:38 -0400

    Fully implement multiply 3 using only addition, subtraction and comparator

--- multiply_1.py
+++ multiply_1.py
@@ -17,21 +17,44 @@ def multiply_2(x,y):
 			x -= 1
 			sum += y
 	return sum
+
 # Create the doubling values before hand
 def createU(x, y):
 	i = 0
 	u = []
 	v = []
-	u[i] = 1
-	v[i] = y
+	u.append(1)
+	v.append(y)
 	while u[i] <= x:
 		i += 1
-		u[i] = u[i - 1] + u[i-1]
-		v[i] = v[i-1]+ v[i-1]
-	return u, v
+		u.append(u[i - 1] + u[i-1])
+		v.append(v[i-1]+ v[i-1])
+	return u, v, i
 
+# Multiplication done only by addition, subtraction and <= 
+#	Case 1 where x >= u[i -1 ]					Case 2 where x < u[i -1 ]
+# |0---------2^i-1---x--------2^i--|			|0-------x--2^i-1--------2^i--|
+#			 u[i-1]			  u[i]							u[i-1]		 u[i]
 def multiply_3(x, y):
+	u, v, j = createU(x,y)
+	i = j
+	a = 0
+	while i != 0:
+		# Case 2 does not satisfy the if
+		i -= 1
+		# Case 1, satisfies the if
+		if u[i] <= x:
+			x -= u[i]
+			a += v[i]
+	return a
 
 
 print(multiply_1(4, 6))
 print(multiply_2(4, 6))
+print(multiply_3(4, 6))
+
+
+
+
+
+
